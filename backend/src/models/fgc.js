b2337535131757a7d5c08160446be37a7fdaf803
gitLrossi <lucasrossiwork@gmail.com>
2026-09-12 18:17:44 -0300

    Ajuste dos modelos + criação do controller e do service

--- backend/src/models/fgc.js
+++ backend/src/models/fgc.js
@@ -47,7 +47,7 @@ const fgcschema = new mongoose.Schema({
     },
     QGC10:{
         type: String,
-        required: [true,"Resposta necessariaa!"]
+        required: [true,"Resposta necessaria!"]
     }
 })
 
