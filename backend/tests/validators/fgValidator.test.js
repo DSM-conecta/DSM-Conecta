c39603f892b7d37e6fbd142851f04d9d04f1b82d
gitLrossi <lucasrossiwork@gmail.com>
2026-09-16 19:02:29 -0300

    ajuste

--- backend/tests/validators/fgValidator.test.js
+++ backend/tests/validators/fgValidator.test.js
@@ -7,16 +7,16 @@ describe('Validação do formulário geral', () => {
         const formulario = {
             name: 'Lucas Rossi',
             email: 'lucas@email.com',
-            QGC1: 'Sim, conhecia bem',
-            QGC2: '5',
-            QGC3: 'O curso',
-            QGC4: 'Muito fáceis',
-            QGC5: 'Sim',
-            QGC6: 'Muito boa',
-            QGC7: 'Sim',
-            QGC8: 'Gostei bastante do site',
-            QGC9: 'Poderia ter mais informações',
-            QGC10: 'Mais informações sobre o mercado de trabalho'
+            QG1: 'Sim, conhecia bem',
+            QG2: '5',
+            QG3: 'O curso',
+            QG4: 'Muito fáceis',
+            QG5: 'Sim',
+            QG6: 'Muito boa',
+            QG7: 'Sim',
+            QG8: 'Gostei bastante do site',
+            QG9: 'Poderia ter mais informações',
+            QG10: 'Mais informações sobre o mercado de trabalho'
         };
 
         expect(validarFormularioG(formulario)).toBe(true);
@@ -26,16 +26,16 @@ describe('Validação do formulário geral', () => {
     const formulario = {
         name: '',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -46,16 +46,16 @@ test('deve rejeitar formulário sem email', () => {
         const formulario = {
             name: 'lucas',
             email: '',
-            QGC1: 'Sim, conhecia bem',
-            QGC2: '5',
-            QGC3: 'O curso',
-            QGC4: 'Muito fáceis',
-            QGC5: 'Sim',
-            QGC6: 'Muito boa',
-            QGC7: 'Sim',
-            QGC8: 'Gostei bastante do site',
-            QGC9: 'Poderia ter mais informações',
-            QGC10: 'Mais informações sobre o mercado de trabalho'
+            QG1: 'Sim, conhecia bem',
+            QG2: '5',
+            QG3: 'O curso',
+            QG4: 'Muito fáceis',
+            QG5: 'Sim',
+            QG6: 'Muito boa',
+            QG7: 'Sim',
+            QG8: 'Gostei bastante do site',
+            QG9: 'Poderia ter mais informações',
+            QG10: 'Mais informações sobre o mercado de trabalho'
         };
 
         expect(validarFormularioG(formulario)).toBe(false);
@@ -65,16 +65,16 @@ test('deve rejeitar formulário sem email', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -84,16 +84,16 @@ test('deve rejeitar QGC1 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: '',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: '',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -104,16 +104,16 @@ test('deve rejeitar QGC2 com nota menor que 1', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '0',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '0',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -124,16 +124,16 @@ test('deve rejeitar QGC2 com nota maior que 5', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '6',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '6',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -144,16 +144,16 @@ test('deve rejeitar QGC2 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -163,16 +163,16 @@ test('deve rejeitar QGC3 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: '',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: '',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -183,16 +183,16 @@ test('deve rejeitar QGC4 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: '',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: '',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -203,16 +203,16 @@ test('deve rejeitar QGC5 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: '',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: '',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -223,16 +223,16 @@ test('deve rejeitar QGC6 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: '',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: '',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -243,16 +243,16 @@ test('deve rejeitar QGC7 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: '',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: '',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -263,16 +263,16 @@ test('deve rejeitar QGC8 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: '',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: '',
+        QG9: 'Poderia ter mais informações',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -283,16 +283,16 @@ test('deve rejeitar QGC9 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: '',
-        QGC10: 'Mais informações sobre o mercado de trabalho'
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: '',
+        QG10: 'Mais informações sobre o mercado de trabalho'
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
@@ -303,16 +303,16 @@ test('deve rejeitar QGC10 vazia', () => {
     const formulario = {
         name: 'Lucas Rossi',
         email: 'lucas@email.com',
-        QGC1: 'Sim, conhecia bem',
-        QGC2: '5',
-        QGC3: 'O curso',
-        QGC4: 'Muito fáceis',
-        QGC5: 'Sim',
-        QGC6: 'Muito boa',
-        QGC7: 'Sim',
-        QGC8: 'Gostei bastante do site',
-        QGC9: 'Poderia ter mais informações',
-        QGC10: ''
+        QG1: 'Sim, conhecia bem',
+        QG2: '5',
+        QG3: 'O curso',
+        QG4: 'Muito fáceis',
+        QG5: 'Sim',
+        QG6: 'Muito boa',
+        QG7: 'Sim',
+        QG8: 'Gostei bastante do site',
+        QG9: 'Poderia ter mais informações',
+        QG10: ''
     };
 
     expect(validarFormularioG(formulario)).toBe(false);
